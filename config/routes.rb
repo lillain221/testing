3ae1fc269705d48562918da520a98e329dedcf9f
Lillain <lillain@LillainnoMBP.usen.ad.jp>
2022-09-06 12:00:46 +0900

    con3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :men
   resources :people
+  resources :properties
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
