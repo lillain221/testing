a7a0c202a9d2207db6d4a8ace44de72cc8fb802e
Lillain <lillain@LillainnoMBP.usen.ad.jp>
2022-09-06 11:10:25 +0900

    con3

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,8 @@
 Rails.application.routes.draw do
-
-  devise_for :users
+  resources :men
+  resources :people
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
-
-
-
-  resources :properties
-
-
-
-
-  get 'cart', to: 'cart#show'
-  post 'cart/add'
-  post 'cart/update'
-  post 'cart/remove'
-  post 'cart/go'
-  get 'cart/confirm'
-  get 'cart/order_summery'
-  get "search/:postal_code" => "search#postal_code"
-  resources :like do
-    member do
-      get :index
-    end
-  end
-  resources :webhooks, only: [:create]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # root "articles#index"
 end
