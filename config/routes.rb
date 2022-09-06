62d7e99b1b81cc948eb544fc725e5669750c0016
Lillain <lillain@LillainnoMBP.usen.ad.jp>
2022-09-06 12:08:18 +0900

    con3

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
 
-  get "search/:postal_code" => "search#postal_code"
+
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
