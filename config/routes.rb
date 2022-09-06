671abcfe690eccb62c9e935e3c2ce695df338251
Lillain <lillain@LillainnoMBP.usen.ad.jp>
2022-09-06 12:08:44 +0900

    con3

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :men
   resources :people
   resources :properties
-  devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
